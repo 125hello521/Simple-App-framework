b22cb44de08a6b5aa42260b030f24a9cf3017fbb
liu-hailin <lhl_012@163.com>
2016-05-24 20:12:10 +0800

    Add files via upload

---
+++ OkHttpUtils.java
@@ -0,0 +1,126 @@
+package com.net.volley;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class OkHttpUtils {
+    private static final MediaType MEDIA_TYPE_PNG = MediaType.parse("image/png");
+    private static final MediaType MEDIA_TYPE_JPG = MediaType.parse("image/jpg");
+    private volatile static OkHttpClient okHttpClient = null;
+    private static Call call;
+
+    private static OkHttpClient getClient() {
+        if (okHttpClient == null) {
+            synchronized (OkHttpUtils.class) {
+                if (okHttpClient == null) {
+                    okHttpClient = new OkHttpClient();
+                    okHttpClient.newBuilder().readTimeout(10, TimeUnit.SECONDS);
+                    okHttpClient.newBuilder().connectTimeout(10, TimeUnit.SECONDS);
+                }
+            }
+        }
+        return okHttpClient;
+    }
+
+    public static void get(String url, RequestParams params) {
+        if (params != null) {
+            // Construct the query string and trim it, in case it
+            // includes any excessive white spaces.
+            ConcurrentHashMap<String, String> map = params.urlParams;
+            // Only add the query string if it isn't empty and it
+            // isn't equal to '?'.
+            if (map != null && !map.isEmpty()) {
+                Iterator iter = map.entrySet().iterator();
+                while (iter.hasNext()) {
+                    Map.Entry<String, String> entry = (Map.Entry<String, String>) iter.next();
+                    url += url.contains("?") ? "&" : "?";
+                    url += entry.getKey() + "=" + entry.getValue();
+                }
+            }
+        }
+        call = getClient().newCall(new Request.Builder().url(url).build());
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+            }
+        });
+    }
+
+    public static void cancelCall() {
+        if (!call.isExecuted()) {
+            call.cancel();
+        }
+    }
+
+    public static void post(String url, RequestParams params) {
+        if (params.haveData()) {
+            RequestBody body = null;
+            if (!params.urlParams.isEmpty() && params.streamParams.isEmpty() && params.fileParams.isEmpty() && params.fileArrayParams.isEmpty()) {
+                FormBody.Builder builder = new FormBody.Builder();
+                Iterator iter = params.urlParams.entrySet().iterator();
+                while (iter.hasNext()) {
+                    Map.Entry<String, String> entry = (Map.Entry<String, String>) iter.next();
+                    builder.add(entry.getKey(), entry.getValue());
+                }
+                body = builder.build();
+            } else {
+                MultipartBody.Builder builder = new MultipartBody.Builder().setType(MultipartBody.FORM);
+                if (!params.urlParams.isEmpty()) {
+                    Iterator iter = params.urlParams.entrySet().iterator();
+                    while (iter.hasNext()) {
+                        Map.Entry<String, String> entry = (Map.Entry<String, String>) iter.next();
+                        builder.addFormDataPart(entry.getKey(), entry.getValue());
+                    }
+                }
+                if (!params.fileParams.isEmpty()) {
+                    Iterator iter = params.urlParams.entrySet().iterator();
+                    while (iter.hasNext()) {
+                        Map.Entry<String, RequestParams.FileWrapper> entry = (Map.Entry<String, RequestParams.FileWrapper>) iter.next();
+                        RequestParams.FileWrapper fileWrapper = entry.getValue();
+                        File file = fileWrapper.file;
+                        if (file.getName().endsWith(".jpg")) {
+                            builder.addFormDataPart(entry.getKey(), file.getName(), RequestBody.create(MEDIA_TYPE_JPG, entry.getValue().file));
+                        }
+                        if (file.getName().endsWith(".png")) {
+                            builder.addFormDataPart(entry.getKey(), file.getName(), RequestBody.create(MEDIA_TYPE_PNG, entry.getValue().file));
+                        }
+
+                    }
+                }
+                body = builder.build();
+            }
+            call = getClient().newCall(new Request.Builder().url(url).post(body).build());
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+
+                }
+            });
+        }
+    }
+}
---
+++ RequestParams.java
@@ -0,0 +1,269 @@
+package com.net.volley;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class RequestParams implements Serializable {
+    public final static String APPLICATION_OCTET_STREAM = "application/octet-stream";
+    public final static String APPLICATION_JSON = "application/json";
+    protected final ConcurrentHashMap<String, String> urlParams = new ConcurrentHashMap<String, String>();
+    protected final ConcurrentHashMap<String, StreamWrapper> streamParams = new ConcurrentHashMap<String, StreamWrapper>();
+    protected final ConcurrentHashMap<String, FileWrapper> fileParams = new ConcurrentHashMap<String, FileWrapper>();
+    protected final ConcurrentHashMap<String, List<FileWrapper>> fileArrayParams = new ConcurrentHashMap<String, List<FileWrapper>>();
+
+    /**
+     * Constructs a new empty {@code RequestParams} instance.
+     */
+    public RequestParams() {
+        this((Map<String, String>) null);
+    }
+
+    /**
+     * Constructs a new RequestParams instance containing the key/value string params from the
+     * specified map.
+     *
+     * @param source the source key/value string map to add.
+     */
+    public RequestParams(Map<String, String> source) {
+        if (source != null) {
+            for (Map.Entry<String, String> entry : source.entrySet()) {
+                put(entry.getKey(), entry.getValue());
+            }
+        }
+    }
+
+    /**
+     * Constructs a new RequestParams instance and populate it with a single initial key/value
+     * string param.
+     *
+     * @param key   the key name for the intial param.
+     * @param value the value string for the initial param.
+     */
+    public RequestParams(final String key, final String value) {
+        this(new HashMap<String, String>() {{
+            put(key, value);
+        }});
+    }
+
+    /**
+     * Adds a key/value string pair to the request.
+     *
+     * @param key   the key name for the new param.
+     * @param value the value string for the new param.
+     */
+    public void put(String key, String value) {
+        if (key != null && value != null) {
+            urlParams.put(key, value);
+        }
+    }
+
+
+    /**
+     * Adds files array to the request.
+     *
+     * @param key   the key name for the new param.
+     * @param files the files array to add.
+     * @throws FileNotFoundException if one of passed files is not found at time of assembling the requestparams into request
+     */
+    public void put(String key, File files[]) throws FileNotFoundException {
+        put(key, files, null, null);
+    }
+
+    /**
+     * Adds files array to the request with both custom provided file content-type and files name
+     *
+     * @param key            the key name for the new param.
+     * @param files          the files array to add.
+     * @param contentType    the content type of the file, eg. application/json
+     * @param customFileName file name to use instead of real file name
+     * @throws FileNotFoundException throws if wrong File argument was passed
+     */
+    public void put(String key, File files[], String contentType, String customFileName) throws FileNotFoundException {
+
+        if (key != null) {
+            List<FileWrapper> fileWrappers = new ArrayList<FileWrapper>();
+            for (File file : files) {
+                if (file == null || !file.exists()) {
+                    throw new FileNotFoundException();
+                }
+                fileWrappers.add(new FileWrapper(file, contentType, customFileName));
+            }
+            fileArrayParams.put(key, fileWrappers);
+        }
+    }
+
+    /**
+     * Adds a file to the request.
+     *
+     * @param key  the key name for the new param.
+     * @param file the file to add.
+     * @throws FileNotFoundException throws if wrong File argument was passed
+     */
+    public void put(String key, File file) throws FileNotFoundException {
+        put(key, file, null, null);
+    }
+
+    /**
+     * Adds a file to the request with custom provided file name
+     *
+     * @param key            the key name for the new param.
+     * @param file           the file to add.
+     * @param customFileName file name to use instead of real file name
+     * @throws FileNotFoundException throws if wrong File argument was passed
+     */
+    public void put(String key, String customFileName, File file) throws FileNotFoundException {
+        put(key, file, null, customFileName);
+    }
+
+    /**
+     * Adds a file to the request with custom provided file content-type
+     *
+     * @param key         the key name for the new param.
+     * @param file        the file to add.
+     * @param contentType the content type of the file, eg. application/json
+     * @throws FileNotFoundException throws if wrong File argument was passed
+     */
+    public void put(String key, File file, String contentType) throws FileNotFoundException {
+        put(key, file, contentType, null);
+    }
+
+    /**
+     * Adds a file to the request with both custom provided file content-type and file name
+     *
+     * @param key            the key name for the new param.
+     * @param file           the file to add.
+     * @param contentType    the content type of the file, eg. application/json
+     * @param customFileName file name to use instead of real file name
+     * @throws FileNotFoundException throws if wrong File argument was passed
+     */
+    public void put(String key, File file, String contentType, String customFileName) throws FileNotFoundException {
+        if (file == null || !file.exists()) {
+            throw new FileNotFoundException();
+        }
+        if (key != null) {
+            fileParams.put(key, new FileWrapper(file, contentType, customFileName));
+        }
+    }
+
+    /**
+     * Adds an input stream to the request.
+     *
+     * @param key    the key name for the new param.
+     * @param stream the input stream to add.
+     */
+    public void put(String key, InputStream stream) {
+        put(key, stream, null);
+    }
+
+    /**
+     * Adds an input stream to the request.
+     *
+     * @param key    the key name for the new param.
+     * @param stream the input stream to add.
+     * @param name   the name of the stream.
+     */
+    public void put(String key, InputStream stream, String name) {
+        put(key, stream, name, null);
+    }
+
+    /**
+     * Adds an input stream to the request.
+     *
+     * @param key         the key name for the new param.
+     * @param stream      the input stream to add.
+     * @param name        the name of the stream.
+     * @param contentType the content type of the file, eg. application/json
+     */
+    public void put(String key, InputStream stream, String name, String contentType) {
+        put(key, stream, name, contentType, true);
+    }
+
+    /**
+     * Adds an input stream to the request.
+     *
+     * @param key         the key name for the new param.
+     * @param stream      the input stream to add.
+     * @param name        the name of the stream.
+     * @param contentType the content type of the file, eg. application/json
+     * @param autoClose   close input stream automatically on successful upload
+     */
+    public void put(String key, InputStream stream, String name, String contentType, boolean autoClose) {
+        if (key != null && stream != null) {
+            streamParams.put(key, StreamWrapper.newInstance(stream, name, contentType, autoClose));
+        }
+    }
+
+    /**
+     * Check if a parameter is defined.
+     *
+     * @param key the key name for the parameter to check existence.
+     * @return Boolean
+     */
+    public boolean has(String key) {
+        return urlParams.get(key) != null ||
+                streamParams.get(key) != null ||
+                fileParams.get(key) != null ||
+                fileArrayParams.get(key) != null;
+    }
+
+    public boolean haveData() {
+        return !urlParams.isEmpty() ||
+                !streamParams.isEmpty() ||
+                !fileParams.isEmpty() ||
+                !fileArrayParams.isEmpty();
+    }
+
+    /**
+     * Removes a parameter from the request.
+     *
+     * @param key the key name for the parameter to remove.
+     */
+    public void remove(String key) {
+        urlParams.remove(key);
+        streamParams.remove(key);
+        fileParams.remove(key);
+        fileArrayParams.remove(key);
+    }
+
+
+    public static class FileWrapper implements Serializable {
+        public final File file;
+        public final String contentType;
+        public final String customFileName;
+
+        public FileWrapper(File file, String contentType, String customFileName) {
+            this.file = file;
+            this.contentType = contentType;
+            this.customFileName = customFileName;
+        }
+    }
+
+    public static class StreamWrapper {
+        public final InputStream inputStream;
+        public final String name;
+        public final String contentType;
+        public final boolean autoClose;
+
+        public StreamWrapper(InputStream inputStream, String name, String contentType, boolean autoClose) {
+            this.inputStream = inputStream;
+            this.name = name;
+            this.contentType = contentType;
+            this.autoClose = autoClose;
+        }
+
+        static StreamWrapper newInstance(InputStream inputStream, String name, String contentType, boolean autoClose) {
+            return new StreamWrapper(
+                    inputStream,
+                    name,
+                    contentType == null ? APPLICATION_OCTET_STREAM : contentType,
+                    autoClose);
+        }
+    }
+}
