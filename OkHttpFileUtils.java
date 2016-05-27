17e453438ec3ae323e8763de1eb6528ccfa63aae
liu-hailin <lhl_012@163.com>
2016-05-27 10:04:55 +0800

    Add files via upload

--- OkHttpFileUtils.java
+++ OkHttpFileUtils.java
@@ -1,7 +1,11 @@
 package com.ejlchina.ejl.utils;
 
+import android.os.Environment;
+import android.util.Log;
+
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -11,18 +15,30 @@ import java.io.OutputStream;
  * 流转化为为文件
  */
 public class OkHttpFileUtils {
-    public static void inputstream2file(InputStream ins, File file) {
+    public static boolean inputstream2file(InputStream ins, File file) {
+        File fD = new File(file.getParent());
+        if (!fD.exists()) {
+            fD.mkdirs();
+        }
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         try {
             OutputStream os = new FileOutputStream(file);
             int bytesRead = 0;
-            byte[] buffer = new byte[8192];
-            while ((bytesRead = ins.read(buffer, 0, 8192)) != -1) {
+            byte[] buffer = new byte[2048];
+            while ((bytesRead = ins.read(buffer, 0, 2048)) != -1) {
                 os.write(buffer, 0, bytesRead);
             }
             os.close();
-            ins.close();
+            return true;
         } catch (Exception e) {
             e.printStackTrace();
+            return false;
         }
     }
 }
